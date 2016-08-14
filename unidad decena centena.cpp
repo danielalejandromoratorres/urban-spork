7f3099feed0cfeb240266c8f52c57d3a0f4f62b2
danielalejandromoratorres <danielalejandro.moratorres@unet.edu.ve>
2016-08-14 17:36:35 -0400

    Add files via upload

---
+++ unidad decena centena.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+ int a,b;
+  while(a/10){
+              b=a%10;
+              a=a/10;
+              printf("%d",b);
+              }
+              printf("%d",a);
+              system("pause");
+              retuern 0;
+              }   
